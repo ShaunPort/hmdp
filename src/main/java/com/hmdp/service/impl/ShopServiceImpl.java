0b7fa0e69c5bd55131afbabb3ac339f1cac2a1e3
Ethan-Yuanxy <947253705@qq.com>
2022-10-21 11:28:27 +0800

    互斥锁-》缓存击穿

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
@@ -28,28 +29,109 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Autowired
     private StringRedisTemplate redisTemplate;
 
-    @Override
-    public Result queryById(Long id) {
+    private boolean tryLock(String key) {
+        Boolean flag =  redisTemplate.opsForValue().setIfAbsent(key, "1", Duration.ofSeconds(RedisConstants.LOCK_SHOP_TTL));
+        return Boolean.TRUE.equals(flag);
+    }
+
+    private void unLock(String key) {
+        redisTemplate.delete(key);
+    }
+
+    /**
+     * 可以防止缓存穿透的查询商铺
+     * @param id
+     * @return
+     */
+    private Shop queryWithPassThrough(Long id) {
         // 1. 查redis
         String shopJson = redisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
         // 2. 命中
         if (shopJson != null && !shopJson.isEmpty()) {
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return shop;
         }
         // 命中 空对象
         if (shopJson != null && shopJson.equals("")) {
-            return Result.fail("商户不存在");
+            return null;
         }
         // 3. 未命中 : 查询数据库
         Shop shop = getById(id);
         if (shop == null) {
             redisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, "", Duration.ofMinutes(RedisConstants.CACHE_NULL_TTL));
-            return Result.fail("商户不存在");
+            return null;
         }
         // 4. 存在即写入redis
         redisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonPrettyStr(shop), Duration.ofMinutes(RedisConstants.CACHE_SHOP_TTL));
         // 5. 返回
+        return shop;
+    }
+
+    /**
+     * 防缓存穿透和击穿
+     * @param id
+     * @return
+     */
+    private Shop queryWithMutex(Long id) {
+        // 1. 查redis
+        String shopJson = redisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
+        // 2. 命中
+        if (shopJson != null && !shopJson.isEmpty()) {
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return shop;
+        }
+        // 命中 空对象
+        if (shopJson != null && shopJson.equals("")) {
+            return null;
+        }
+        // 3. 未命中 : 查询数据库    -》 重建缓存
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        // 3.1 尝试获取互斥锁
+        while (!tryLock(lockKey)) {
+            // 3.2 失败就休眠并重试获取互斥锁
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                unLock(lockKey);
+            }
+        }
+        // 4. 再次检测是否已经缓存重建
+        shopJson = redisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
+        if (shopJson != null && !shopJson.isEmpty()) {
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            unLock(lockKey);
+            return shop;
+        }
+        if (shopJson != null && shopJson.equals("")) {
+            unLock(lockKey);
+            return null;
+        }
+        // 开始重建
+        // 5. 查数据库
+        Shop shop = getById(id);
+        if (shop == null) {
+            redisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, "", Duration.ofMinutes(RedisConstants.CACHE_NULL_TTL));
+            return null;
+        }
+        // 6. 存在即写入redis
+        redisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonPrettyStr(shop), Duration.ofMinutes(RedisConstants.CACHE_SHOP_TTL));
+        // 6.1 施放锁
+        unLock(lockKey);
+        // 7. 返回
+        return shop;
+    }
+
+    @Override
+    public Result queryById(Long id) {
+        // 查询
+        // Shop shop = queryWithPassThrough(id);
+        Shop shop = queryWithMutex(id);
+        if (shop == null) {
+            return Result.fail("商铺不存在");
+        }
+        // 6. 返回
         return Result.ok(shop);
     }
 
